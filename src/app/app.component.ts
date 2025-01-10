ce83ada7902e74e80784274e8c6cb764d3948598
fadoua-D <derouich.fadoua@gmail.com>
2025-01-10 15:04:55 +0100

    add map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MapComponent} from './map/map.component';
+// import { RouterOutlet } from '@angular/router';
+// import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  //imports: [FooterComponent, RouterOutlet],
+  //imports: [RouterOutlet],
+  imports: [
+    MapComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
